a94d207a6bfe9554b6ed99913afb07c707c9f125
unknown <programacionluxury@gmail.com>
2026-07-22 21:08:21 -0400

    Arregle el script hello-millonaires

--- Hello-Millonaires.py
+++ Hello-Millonaires.py
@@ -3,7 +3,7 @@ name = input(f"Hi, what your name?:" )
 comensar = input("{name} quieres ser millonario?(si/no):" )
 
 if comensar.lower() == "si":
-    print(f"Si puedes serlo solo que tienes que creertelo y pensar como ellos y hablar english por los millonair@s hablan english.")
-
+    print(f"Si puedes serlo solo que tienes que creertelo y pensar como ellos y hablar english porque los millonair@s hablan english.")
+    print('La cleve del exito es tener constancia')
 else:
     print(f"{name} si tienes que ser millonario,\nno digas que no  ")    
