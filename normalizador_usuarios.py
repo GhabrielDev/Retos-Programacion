86f9c6e2f729dc7726e9d7e2435f5edaf6c2fcb7
unknown <programacionluxury@gmail.com>
2026-07-31 07:36:17 -0400

    Actualizan normalizador y agregar otra funcion

--- normalizador_usuarios.py
+++ normalizador_usuarios.py
@@ -1,6 +1,13 @@
 name = input("Ingresa tu Nombre: ")
+gmail = input("Ingresa tu gmail: ")
+
 def arreglar_name(name):
-    texto_limpio = name.strip()
-    final = texto_limpio.title()
-    return final
+    return name.strip().title().replace(" ","")
+
+def arreglar_gmail(gmail):
+    return gmail.lower().replace(" ","")
+
+print("----- Datos del Usuario -----")
+print(arreglar_gmail(gmail))
 print(arreglar_name(name))
+print(30 * "-")
